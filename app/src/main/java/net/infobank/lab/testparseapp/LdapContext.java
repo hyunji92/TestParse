95285f2d61c8dced3a6adc380b9a8c5ba26e1048
Á¤ÇöÁö(chunghj09) <chunghj09@infobank.net>
2015-07-08 18:21:40 +0900

    유효성 확인위한 라이브러리 사용 확인 필요.... LDAPContext 가 필요하다.

---
+++ app/src/main/java/net/infobank/lab/testparseapp/LdapContext.java
@@ -0,0 +1,7 @@
+package net.infobank.lab.testparseapp;
+
+/**
+ * Created by chunghj on 15. 7. 8..
+ */
+public interface LdapContext {
+}
--- app/src/main/java/net/infobank/lab/testparseapp/LoginActivity.java
+++ app/src/main/java/net/infobank/lab/testparseapp/LoginActivity.java
@@ -1,6 +1,7 @@
 package net.infobank.lab.testparseapp;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
@@ -9,7 +10,9 @@ import android.widget.EditText;
 
 import com.unboundid.ldap.sdk.LDAPConnection;
 import com.unboundid.ldap.sdk.LDAPException;
+import com.unboundid.ldap.sdk.migrate.ldapjdk.LDAPAttributeSet;
 
+import java.util.Hashtable;
 import java.util.Properties;
 
 /**
@@ -24,6 +27,10 @@ public class LoginActivity extends Activity implements View.OnClickListener {
     private final int port = 389;
     //private LDAPConnection ldapConnection;
 
+    //LDAP Context
+    //LDAPCon context = null;
+    private Context context = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,11 +41,14 @@ public class LoginActivity extends Activity implements View.OnClickListener {
         mIbEmail = (EditText) findViewById(R.id.et_email_id);
         mIbPassword = (EditText) findViewById(R.id.et_password);
         mLoginBtn = (Button) findViewById(R.id.btn_login);
+        mLoginBtn.setOnClickListener(this);
+
 
         String email = mIbEmail.getText().toString();
         String password = mIbPassword.getText().toString();
 
-        mLoginBtn.setOnClickListener(this);
+        String path = String.format(GlobalConstant.PROVIDER_URL,GlobalConstant.INITIAL_CONTEXT_FACTORY);
+        String filterString = "(CN=LDAP)";
 
 
     }
@@ -56,15 +66,55 @@ public class LoginActivity extends Activity implements View.OnClickListener {
 
         // 여기서 저정보들을 연결할때 넘긴다?
         // LDAPConnection ldapConnection  = new LDAPConnection(env, null);
+        // LdapContext connection = new InitialLdapContext(env, null);
+
+        LDAPAttributeSet ldapAttributeSet = new LDAPAttributeSet();
+        //ldapAttributeSet.
 
         LDAPConnection connection = new LDAPConnection();
         try {
+
+            //context = new InitialDirContext(env);
+            //SearchControls searcher = new SearchControls();
+
             connection.connect(GlobalConstant.PROVIDER_URL, port);
+            //connection.
             //connection.connect(3, GlobalConstant.PROVIDER_URL, port,
             //GlobalConstant.SECURITY_PRINCIPAL, GlobalConstant.SECURITY_CREDENTIALS);
 
         } catch (LDAPException e) {
             e.printStackTrace();
+        }
+
+        /**
+         * LDAP 계정과 암호를 이용한 사용자 인증
+         *
+         * @param userId
+         *            계정명
+         * @param password
+         *            암호
+         * @return 인증 여부 (ID / PASS 가 일치하는지 아닌지를 확인함)
+         */
+        public static boolean isAuthenticatedUser( mIbEmail,  mIbPassword) {
+            boolean isAuthenticated = false;
+            //String path = String.format(ldapPathFormat, ldapDomain);
+            if (mIbEmail != null &&  mIbPassword != "") {
+                Hashtable<String, String> properties = new Hashtable<String, String>();
+                env.put(GlobalConstant.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.ldap.LdapCtxFactory");
+                env.put(GlobalConstant.PROVIDER_URL, "ldap://infoad01.infobank.net:389");
+                env.put(GlobalConstant.SECURITY_PRINCIPAL,
+                        "CN=LDAP,OU=Server,OU=Service,DC=infobank,DC=net");
+                env.put(GlobalConstant.SECURITY_CREDENTIALS, "P@ssword11");
+                env.put("java.naming.ldap.version", "3");
+
+                try {
+                    LDAPContext con = new InitialLDAPContext(properties);
+                    isAuthenticated = true;
+                    con.close();
+                } catch (NamingException e) {
+                }
+            }
+            return isAuthenticated;
         }
 
 
